a887df81c94e479c9a979736ebc6a4c547efcb6b
balbiss <balbis2050@gmail.com>
2026-03-03 09:38:25 -0300

    fix(frontend): import useRef in useAuth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState, useRef } from 'react';
 import { supabase } from '@/lib/supabase';
 import { Session, User } from '@supabase/supabase-js';
 
